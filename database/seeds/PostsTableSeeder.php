86fef2abb7b2a1fb2acb484447ea36ca915982d3
Elvis Silva <slv_evs@yahoo.com.br>
2016-02-09 02:33:54 -0200

    criacao do seeder e factory de tags / comentario no seeder de posts

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,7 +12,7 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        Post::truncate();
+        //Post::truncate(); // comentado para não gerar erro co as chaves extrangeiras
 
         factory('App\Post', 15)->create();
 
